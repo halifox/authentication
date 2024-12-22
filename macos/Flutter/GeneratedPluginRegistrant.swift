e2a23dd0930a272864b2c2ff5c16a00f8f5cbebd
halifox <106858061+halifox@users.noreply.github.com>
2024-12-22 19:18:17 +0800

    feat: added new plugins and dependencies
    - Added dynamic_color plugin registration in Linux and Windows.
    - Added file_selector_linux and file_selector_windows plugin registrations.
    - Added url_launcher_linux and url_launcher_windows plugin registrations.
    - Updated pubspec.yaml to include new dependencies and version updates.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import dynamic_color
+import file_selector_macos
+import path_provider_foundation
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
