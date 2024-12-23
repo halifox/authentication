cae0e2fddf43c3bb89dab66033465b4a10588f3a
halifox <106858061+halifox@users.noreply.github.com>
2024-12-23 23:36:50 +0800

    fix: updated authentication configuration
    - Replaced Sembast import with OTP support.
    - Added multiple test authentication configurations for different services.
    - Updated debug mode to populate database with sample OTP configurations.

--- lib/auth_repository.dart
+++ lib/auth_repository.dart
@@ -6,7 +6,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:purity_auth/auth.dart';
 import 'package:purity_auth/encrypt_codec.dart';
-import 'package:sembast/sembast.dart';
+import 'package:purity_auth/otp.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:sembast_web/sembast_web.dart';
 
@@ -46,8 +46,13 @@ class AuthRepositoryImpl extends AuthRepository {
     final Database db = await _authDB;
     if (kDebugMode) {
       await store.delete(db);
-      upsert(AuthenticationConfig(secret: 'JQSR2VNH75AMLYRV'));
-      upsert(AuthenticationConfig(secret: 'JQSR2VNH75AMLYRV', type: Type.hotp));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.totp, account: "user@github.com", issuer: "GitHub", intervalSeconds: 30));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.totp, account: "user@gmail.com", issuer: "Google", intervalSeconds: 30));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.totp, account: "user@icloud.com", issuer: "Apple", intervalSeconds: 40));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.totp, account: "user@dropbox.com", issuer: "Dropbox", intervalSeconds: 45));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.totp, account: "user@1dot1dot1dot1.com", issuer: "1dot1dot1dot1", intervalSeconds: 60));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.hotp, account: "user@aws.com", issuer: "Amazon", counter: 0));
+      upsert(AuthenticationConfig(secret: OTP.randomSecret(), type: Type.hotp, account: "user@ansible.com", issuer: "ansible", counter: 0));
     }
   }
 
