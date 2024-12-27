fea069853e6ebec9b89b4f38731f14819cad2547
halifox <106858061+halifox@users.noreply.github.com>
2024-12-27 23:04:55 +0800

    feat: added biometric preferences handling
    - Added new prefs for biometric unlock and captcha settings.
    - Introduced signal-based shared preferences wrapper.
    - Created Prefs class to manage app-specific preferences.

---
+++ lib/prefs.dart
@@ -0,0 +1,16 @@
+import 'package:purity_auth/signal_shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:signals_flutter/signals_flutter.dart';
+
+class Prefs {
+  final SharedPreferences sp;
+
+  Prefs({required this.sp});
+
+  late Signal<bool> biometricUnlock = SignalSharedPreferences<bool>('biometricUnlock', false, sp);
+  late Signal<bool> isShowCaptchaOnTap = SignalSharedPreferences<bool>('isShowCaptchaOnTap', false, sp);
+  late Signal<bool> isCopyCaptchaOnTap = SignalSharedPreferences<bool>('isCopyCaptchaOnTap', true, sp);
+  late Signal<bool> disableScreenshot = SignalSharedPreferences<bool>('disableScreenshot', true, sp);
+
+  void dispose() {}
+}
